d3763cea5f3b970ce99bfbb495b14f045a8fb337
PhSpring <info@phspring.nl>
2015-02-17 02:15:20 +0100

    BugFix

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,5 +3,7 @@
 use PhSpring\ZF1\Application\Bootstrap as ZF1Bootstrap;
 
 class Bootstrap extends ZF1Bootstrap {
-
+    public function __construct($application) {
+        parent::__construct($application);
+    }
 }
